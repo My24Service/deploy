64c3fee2bd82d4dbab4dc7b0342671615372b176
Richard Jansen <richard@pedroja.tech>
2022-01-03 11:31:54 +0100

    removed deploy backend from repo

--- deploy/deploy/settings.py
+++ deploy/deploy/settings.py
@@ -1,24 +1,10 @@
 from pathlib import Path
-import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_DIR = lambda base: os.path.abspath(os.path.join(os.path.dirname(__file__), '../', base).replace('\\', '/'))
 SECRET_KEY = 'django-insecure-x@7=l%*rn%!8t19uoa12_8fdkq5n@qjs#37yfkpfaa_2%y3$qp'
 DEBUG = False
-STATIC_ROOT = PROJECT_DIR('static')
-STATIC_URL = '/static/'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
-    os.path.join(BASE_DIR, 'my24frontend', 'dist'),
-)
-
 ALLOWED_HOSTS = []
-INSTALLED_APPS = ['app', 'django.contrib.staticfiles']
+INSTALLED_APPS = ['app']
 MIDDLEWARE = []
 ROOT_URLCONF = 'deploy.urls'
 TEMPLATES = []
