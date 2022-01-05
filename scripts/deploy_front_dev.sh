c3946fa16a3a067698332add94c1742f2a937805
Richard Jansen <richard@pedroja.tech>
2022-01-05 15:05:44 +0100

    execute collecstatic after deploy

--- scripts/deploy_front_dev.sh
+++ scripts/deploy_front_dev.sh
@@ -2,3 +2,13 @@
 
 cd deploy/deploy
 ../venv/bin/python manage.py deploy_front_dev
+
+# collect static
+cd ${MY24_BASEDIR}/live/release
+
+# activate virtualenv
+source venv/bin/activate
+
+cd source
+
+python manage.py collectstatic --noinput --clear
