53c2e5e13073b899be3397fe8ed73ef1c3104e92
Richard Jansen <richard@pedroja.tech>
2022-06-28 22:00:21 +0200

    rust deploy dirs

--- deploy/deploy_rust.sh
+++ deploy/deploy_rust.sh
@@ -16,4 +16,4 @@ DEPLOY_DIR="${MY24_BASEDIR}/live/${RELEASE_NAME}"
 echo "DEPLOY_DIR: $DEPLOY_DIR"
 
 # set symlink
-rm -fr "${MY24_BASEDIR}/live/release/source/my24frontend" && ln -s "${DEPLOY_DIR}" "${MY24_BASEDIR}/live/release/source/my24frontend"
+rm -fr "${MY24_BASEDIR}/live/rust-backend" && ln -s "${DEPLOY_DIR}" "${MY24_BASEDIR}/live/rust-backend"
