6ca231679aec29bdb08e7e664983ceb62007001f
Richard Jansen <demo_epso@proton.me>
2026-04-25 15:31:43 +0200

    setuptools; remove rust part

--- scripts/deploy_live.sh
+++ scripts/deploy_live.sh
@@ -4,4 +4,5 @@ rm -f $HOME/.aws/credentials
 ln -s $HOME/.aws/credentials-linode $HOME/.aws/credentials
 
 cd /var/www/my24service/live/release/source && . ../venv/bin/activate
+pip install --upgrade setuptools
 ./manage.py deploy_live
--- scripts/deploy_rust_live.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -f $HOME/.aws/credentials
-ln -s $HOME/.aws/credentials-linode $HOME/.aws/credentials
-
-cd /var/www/my24service/docker/
-
-./update.sh
