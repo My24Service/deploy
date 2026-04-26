912924aaac6ce71f21f0b36df3a7a0f49d7acc8a
Richard Jansen <demo_epso@proton.me>
2026-04-26 19:40:26 +0200

    scp frontend after deploy

--- scripts/deploy_front_dev.sh
+++ scripts/deploy_front_dev.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+source /etc/profile.d/frontend-host.sh
+
 rm -f $HOME/.aws/credentials
 ln -s $HOME/.aws/credentials-linode $HOME/.aws/credentials
 
 cd deploy/deploy
 ../venv/bin/python manage.py deploy_front_dev
+
+scp -r /var/www/my24service/live/frontend-release/dist/* $FRONTEND_HOST_PATH
